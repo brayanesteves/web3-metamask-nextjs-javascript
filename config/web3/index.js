40f1923173bbaea3e8e5f9a525cc274dc7dfc1cc
Brayan Esteves <werneriasearch@gmail.com>
2022-01-31 10:25:20 -0400

    ✔️Web3✔️ - Metamask

---
+++ config/web3/index.js
@@ -0,0 +1,11 @@
+import web3 from 'web3';
+import { InjectedConnector } from '@web3-react/injected-connector';
+
+const ETHETREUM_NETWORK_ID = 1;
+export const connector = new InjectedConnector({
+    supportedChainIds: [ETHETREUM_NETWORK_ID]
+});
+export const getLibrary = (provider) => {
+    const library = new web3(provider);
+    return library;
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+import { getLibrary } from '../config/web3/index';
+import { Web3ReactProvider } from '@web3-react/core';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<Web3ReactProvider getLibrary={getLibrary}>
+    <Component {...pageProps} />
+  </Web3ReactProvider>)
 }
 
 export default MyApp
